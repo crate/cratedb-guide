e0b07d12f71e1501790b945d195d512ef86d50c9
Andreas Motl <andreas.motl@crate.io>
2025-09-18 02:57:05 +0200

    Chore: Convert `integrate/**` into MyST format

---
+++ docs/integrate/oracle/init.sql
@@ -0,0 +1,6 @@
+DROP TABLE demo;
+CREATE TABLE IF NOT EXISTS demo (id LONG, temperature FLOAT, humidity FLOAT);
+INSERT INTO demo (id, temperature, humidity) VALUES (1, 42.84, 83.1);
+INSERT INTO demo (id, temperature, humidity) VALUES (2, 84.84, 56.99);
+SELECT * FROM demo;
+exit;
